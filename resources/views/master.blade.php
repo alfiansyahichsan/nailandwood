3e69c4dc20fed2bfc5269e3a10db7b864f29f200
HEYSATAN\HEYSATAN <alfiansyah.ichsan@gmail.com>
2017-07-19 03:43:55 +0700

    new

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,13 +16,25 @@
 </head>
 <body>
 	<div class="page-loader">
-		<div class="spinner">
-			<div class="rect1"></div>
-			<div class="rect2"></div>
-			<div class="rect3"></div>
-			<div class="rect4"></div>
-			<div class="rect5"></div>
+		<div class="loader">
+	<div class="loader-inner">
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
 		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+	</div>
+</div>
 	</div>
 @yield('layout')
 @yield('script')
